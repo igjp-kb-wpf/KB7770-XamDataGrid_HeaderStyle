512c24125ba0e3d0c28420abdf7efa96b2fef261
IG-Mori <MMekaru@infragistics.com>
2024-12-25 12:16:07 +0900

    Initial commit
    Initial commit

---
+++ KB7770-XamDataGrid_HeaderStyle/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace KB7770_XamDataGrid_HeaderStyle
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ KB7770-XamDataGrid_HeaderStyle/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace KB7770_XamDataGrid_HeaderStyle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // データを設定
+            var data = new List<Book>
+            {
+                new Book { Id = 0, Title = "枕草子", Author = "清少納言", Excerpt = "春はあけぼの。" },
+                new Book { Id = 1, Title = "方丈記", Author = "鴨長明", Excerpt = "行く川のながれ" },
+                new Book { Id = 2, Title = "徒然草", Author = "吉田兼好", Excerpt = "つれづれなるままに" },
+            };
+            DataGrid.DataSource = data;
+        }
+    }
+
+    public class Book
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Excerpt { get; set; }
+    }
+}
